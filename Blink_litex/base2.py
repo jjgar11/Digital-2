b38f1e9b2f28e945940aec444fc715095d9208cc
Juan J. Garcia <juanjgarcialeon@gmail.com>
2023-08-25 20:35:10 -0500

    Otro archivo base que sirve como guia

---
+++ Blink_litex/base2.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python3
+
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Build/Use ----------------------------------------------------------------------------------------
+#
+# 1) SoC with regular UART and optional Ethernet connected to the CPU:
+# Connect a USB/UART to J19: TX of the FPGA is DATA_LED-, RX of the FPGA is KEY+.
+# ./colorlight_5a_75x.py --revision=7.0 (or 6.1)  --build (--with-ethernet to add Ethernet capability)
+# Note: on revision 6.1, add --uart-baudrate=9600 to lower the baudrate.
+# ./colorlight_5a_75x.py --load
+# You should see the LiteX BIOS and be able to interact with it.
+#
+# 2) SoC with UART in crossover mode over Etherbone:
+# ./colorlight_5a_75x.py --revision=7.0 (or 6.1) --uart-name=crossover --with-etherbone --csr-csv=csr.csv --build
+# ./colorlight_5a_75x.py --load
+# ping 192.168.1.50
+# Get and install wishbone tool from: https://github.com/litex-hub/wishbone-utils/releases
+# wishbone-tool --ethernet-host 192.168.1.50 --server terminal --csr-csv csr.csv
+# You should see the LiteX BIOS and be able to interact with it.
+#
+# 3) SoC with USB-ACM UART (on V7.0):
+# - Replace U23 with a SN74CBT3245APWR or remove U23 and place jumper wires to make the ports bi-directional.
+# - Place a 15K resistor between J4 pin 2 and J4 pin 4.
+# - Place a 15K resistor between J4 pin 3 and J4 pin 4.
+# - Place a 1.5K resistor between J4 pin 1 and J4 pin 3.
+# - Connect USB DP (Green) to J4 pin 3, USB DN (White) to J4 pin 2.
+# ./colorlight_5a_75x.py --revision=7.0 --uart-name=usb_acm  --build
+# ./colorlight_5a_75x.py --load
+# You should see the LiteX BIOS and be able to interact with it.
+#
+# Note that you can also use a 5A-75E board:
+# ./colorlight_5a_75x.py --board=5a-75e --revision=7.1 (or 6.0) --build
+#
+# Disclaimer: SoC 2) is still a Proof of Concept with large timings violations on the IP/UDP and
+# Etherbone stack that need to be optimized. It was initially just used to validate the reversed
+# pinout but happens to work on hardware...
+
+from migen import *
+from migen.genlib.resetsync import AsyncResetSynchronizer
+
+from litex.gen import *
+
+from litex.build.io import DDROutput
+
+from litex_boards.platforms import alpha_board
+
+from litex.soc.cores.clock import *
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+from litex.soc.cores.led import LedChaser
+
+from litedram.modules import M12L16161A, M12L64322A
+from litedram.phy import GENSDRPHY, HalfRateGENSDRPHY
+
+from liteeth.phy.ecp5rgmii import LiteEthPHYRGMII
+
+from ios import Led
+from litex.build.generic_platform import IOStandard, Subsignal, Pins
+
+# IOs ------------------------------------------------------------------------
+_leds = [
+    ("user_led", 0, Pins("T6"), IOStandard("LVCMOS33")),  # LED en la placa
+    ("user_led", 1, Pins("F3"), IOStandard("LVCMOS33")),  # LED externo
+]
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class _CRG(LiteXModule):
+    def __init__(self, platform, sys_clk_freq, use_internal_osc=False, with_usb_pll=False, with_rst=True, sdram_rate="1:1"):
+        self.rst    = Signal()
+        self.cd_sys = ClockDomain()
+        if sdram_rate == "1:2":
+            self.cd_sys2x    = ClockDomain()
+            self.cd_sys2x_ps = ClockDomain()
+        else:
+            self.cd_sys_ps = ClockDomain()
+
+        # # #
+
+        # Clk / Rst
+        if not use_internal_osc:
+            clk = platform.request("clk25")
+            clk_freq = 25e6
+        else:
+            clk = Signal()
+            div = 5
+            self.specials += Instance("OSCG",
+                                p_DIV = div,
+                                o_OSC = clk)
+            clk_freq = 310e6/div
+
+        rst_n = 1 if not with_rst else platform.request("user_btn_n", 0)
+
+        # PLL
+        self.pll = pll = ECP5PLL()
+        self.comb += pll.reset.eq(~rst_n | self.rst)
+        pll.register_clkin(clk, clk_freq)
+        pll.create_clkout(self.cd_sys,    sys_clk_freq)
+        if sdram_rate == "1:2":
+            pll.create_clkout(self.cd_sys2x,    2*sys_clk_freq)
+            pll.create_clkout(self.cd_sys2x_ps, 2*sys_clk_freq, phase=180) # Idealy 90° but needs to be increased.
+        else:
+           pll.create_clkout(self.cd_sys_ps, sys_clk_freq, phase=180) # Idealy 90° but needs to be increased.
+
+        # USB PLL
+        if with_usb_pll:
+            self.usb_pll = usb_pll = ECP5PLL()
+            self.comb += usb_pll.reset.eq(~rst_n | self.rst)
+            usb_pll.register_clkin(clk, clk_freq)
+            self.cd_usb_12 = ClockDomain()
+            self.cd_usb_48 = ClockDomain()
+            usb_pll.create_clkout(self.cd_usb_12, 12e6, margin=0)
+            usb_pll.create_clkout(self.cd_usb_48, 48e6, margin=0)
+
+        # SDRAM clock
+        sdram_clk = ClockSignal("sys2x_ps" if sdram_rate == "1:2" else "sys_ps")
+        self.specials += DDROutput(1, 0, platform.request("sdram_clock"), sdram_clk)
+
+# BaseSoC ------------------------------------------------------------------------------------------
+
+class BaseSoC(SoCCore):
+    def __init__(self, 
+        sys_clk_freq     = 60e6,
+        with_ethernet    = True,
+        eth_phy          = 0,
+        with_led_chaser  = True,
+        use_internal_osc = False,
+        sdram_rate       = "1:1",
+        **kwargs):
+        platform = alpha_board.Platform()
+        platform.add_extension(_leds)
+
+        if with_ethernet:
+            assert use_internal_osc, "You cannot use the 25MHz clock as system clock since it is provided by the Ethernet PHY and will stop during PHY reset."
+
+        # Clock Reset Generation --------------------------------------------------------------------------------------
+        with_rst     = kwargs["uart_name"] not in ["serial", "crossover"] # serial_rx shared with user_btn_n.
+        with_usb_pll = kwargs.get("uart_name", None) == "usb_acm"
+        self.crg = _CRG(platform, sys_clk_freq,
+            use_internal_osc = use_internal_osc,
+            with_usb_pll     = with_usb_pll,
+            with_rst         = with_rst,
+            sdram_rate       = sdram_rate
+        )
+
+        # SoCCore ----------------------------------------------------------------------------------
+        SoCCore.__init__(
+            self, platform, 
+            int(sys_clk_freq), 
+            ident="LiteX SoC alpha_board", 
+            **kwargs)
+
+        # SDR SDRAM --------------------------------------------------------------------------------
+        if not self.integrated_main_ram_size:
+            sdrphy_cls = HalfRateGENSDRPHY if sdram_rate == "1:2" else GENSDRPHY
+            self.sdrphy = sdrphy_cls(platform.request("sdram"), sys_clk_freq)
+            sdram_cls  = M12L64322A
+            self.add_sdram("sdram",
+                phy                     = self.sdrphy,
+                module                  = sdram_cls(sys_clk_freq, sdram_rate),
+                l2_cache_size           = kwargs.get("l2_size", 8192),
+                l2_cache_full_memory_we = False,
+
+            )
+
+        # Ethernet / Etherbone ---------------------------------------------------------------------
+        if with_ethernet:
+            self.ethphy = LiteEthPHYRGMII(
+                clock_pads = self.platform.request("eth_clocks", eth_phy),
+                pads       = self.platform.request("eth", eth_phy),
+                tx_delay   = 0e-9)
+            self.add_ethernet(phy=self.ethphy, data_width=32)
+
+        # Leds -------------------------------------------------------------------------------------
+        user_leds = Cat(*[platform.request("user_led", i) for i in range(1)])
+        self.submodules.leds = Led(user_leds)
+        self.add_csr("leds")
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    from litex.build.parser import LiteXArgumentParser
+    parser = LiteXArgumentParser(platform=alpha_board.Platform, description="LiteX SoC on Colorlight 5A-75X.")
+    parser.add_target_argument("--sys-clk-freq",      default=60e6, type=float, help="System clock frequency.")
+    ethopts = parser.target_group.add_mutually_exclusive_group()
+    ethopts.add_argument("--with-ethernet",           action="store_true",    help="Enable Ethernet support.")
+    parser.add_target_argument("--eth-phy",           default=0, type=int,    help="Ethernet PHY (0 or 1).")
+    parser.add_target_argument("--use-internal-osc",  action="store_true",    help="Use internal oscillator.")
+    parser.add_target_argument("--sdram-rate",        default="1:1",          help="SDRAM Rate (1:1 Full Rate or 1:2 Half Rate).")
+    args = parser.parse_args()
+
+    soc = BaseSoC(
+        sys_clk_freq     = args.sys_clk_freq,
+        with_ethernet    = args.with_ethernet,
+        eth_phy          = args.eth_phy,
+        use_internal_osc = args.use_internal_osc,
+        sdram_rate       = args.sdram_rate,
+        **parser.soc_argdict
+    )
+    builder = Builder(soc, **parser.builder_argdict)
+
+    if args.build:
+        builder.build(**parser.toolchain_argdict)
+
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(builder.get_bitstream_filename(mode="sram", ext=".svf")) # FIXME
+
+if __name__ == "__main__":
+    main()
