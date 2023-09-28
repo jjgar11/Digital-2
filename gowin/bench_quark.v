2c2c2981933f94907a5628149e3ac02a7c60ca50
Juan J. Garcia <juanjgarcialeon@gmail.com>
2023-09-28 09:24:18 -0500

    Carpeta Camargo

---
+++ gowin/bench_quark.v
@@ -0,0 +1,46 @@
+
+module bench();
+   parameter tck              = 40;
+
+   reg CLK;
+   reg i;
+   reg RESET; 
+   wire [4:0] LEDS;
+   reg  RXD = 1'b0;
+   wire TXD;
+
+   SOC uut(
+     .clk(CLK),
+     .resetn(RESET),
+     .LEDS(LEDS),
+     .RXD(RXD),
+     .TXD(TXD)
+   );
+
+
+initial         CLK <= 0;
+always #(tck/2) CLK <= ~CLK;
+
+
+   reg[4:0] prev_LEDS = 0;
+   initial begin
+	 if(LEDS != prev_LEDS) begin
+	    $display("LEDS = %b",LEDS);
+	 end
+	 prev_LEDS <= LEDS;
+	
+   end
+   
+   initial
+ begin
+    $dumpfile("bench.vcd");
+    $dumpvars(0,bench);
+    #0   RESET = 1;
+    #80  RESET = 1;
+    #160 RESET = 0;
+    #(tck*1000000) $finish;
+ end
+ 
+ 
+endmodule   
+ 
---
+++ gowin/c_firmware/hello.c
@@ -0,0 +1,18 @@
+#include <femtorv32.h>
+
+int main() {
+    int k = 0;
+     /* Using: "libfemtorv/include/HardwareConfig_bits.h" (derived from "HardwareConfig_bits.v")
+      * IO_XXX = 1 << (IO_XXX_bit + 2); IO_LEDS_bit=0; --> (1<<(0+2))=4 --> 0x400004
+        delay use FEMTORV32_FREQ to calculate delay, uou must change it to adjust you frequency libfemtorv/include/femtorv32.h
+      */
+    
+     printf("\n\rHello C World!\n\r");
+     while(1){
+       delay(200);
+       k++;
+       *(volatile uint32_t*)(0x400004) = ~k;
+     }	
+   return 0;
+}
+
